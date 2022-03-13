a20c4686a69da0b55f0266c37107b4eab526976c
Carnoval 15 <carnoval@protonmail.com>
2022-03-13 21:07:38 +0330

    yo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Yes() {
-  return (
-    <div>
-      <header>
-        <nav>
-          <img src={logo} className="App-logo" alt="logo" />
-        </nav>
-      </header>
-      <h1>Reasons I'm excited to learn React!</h1>
-      <ol>
-        <li>react is react</li>
-        <li>react is wonderful</li>
-        <li>well, uh ran out of ideas, F</li>
-      </ol>
-      <footer>
-        @ 2022 Carnoval development/.
-      </footer>
-    </div>
-  )
-}
-function App(props) {
-  const subject = "yes";
-  return (
-    <div className="App">
+import Navbar from "./Components/Navbar.js"
+import Main from "./Components/Main.js"
+//import Footer from "./Components/Footer.js"
 
+
+function App(props) {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
     </div>
   );
 }
 
 export default App;
+
+
+// function Yes() {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Main />
+//       <Footer />
+//     </div>
+//   )
+// }
---
+++ src/Components/Main.js
@@ -0,0 +1,8 @@
+import React from "react"
+import logo from './logo.svg';
+
+export default function MainContent() {
+    return (
+        <h1>Main Content</h1>
+    )
+}
